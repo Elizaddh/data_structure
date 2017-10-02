2d2b45a2d02158bdae557c06987495f3954bc309
DEliza <32403146+Elizaddh@users.noreply.github.com>
2017-10-01 22:07:00 -0500

    Add files via upload
    python loop basic

---
+++ loop.py
@@ -0,0 +1,13 @@
+#a very simple loop
+for i in range(0,10): 
+        print i
+print 'for-loop done'
+
+#a very simple while loop
+i=0
+while i<10: 
+    print i
+    i=i+1
+print 'loop is done'
+
+    
